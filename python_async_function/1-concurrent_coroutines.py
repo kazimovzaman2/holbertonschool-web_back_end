ba637f5473a36f15fd0760dd5243f7e94f211690
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-29 15:18:31 +0400

    added docstring

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Wait for a random delay between 0 and `max_delay`."""
     delays = []
     for _ in range(n):
         delays.append(await wait_random(max_delay))
