ea3c660fc444c6324f96c29ac6be66e44b9c76d2
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-29 15:05:10 +0400

    Task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python async task"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int=10) -> float:
+    """"""
+    value = random.uniform(0, max_delay)
+    await asyncio.sleep(value)
+    return value
