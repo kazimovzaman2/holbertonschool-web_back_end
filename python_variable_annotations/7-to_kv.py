7680d0d11807d5b3fbbc7145a2999ab83ccf6e6b
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-29 14:08:53 +0400

    Tuple needs to be get from typing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Python annotation task"""
-from typing import List, Union
+from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of a string and a number squared"""
     return (k, v ** 2)
