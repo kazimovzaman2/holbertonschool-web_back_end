76e680db555866161126e0d9b8ca4a0b242fb527
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-29 07:41:05 +0400

    Task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Python annotation task"""
+from  typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """"""
+    n = 0
+    for i in input_list:
+        n += i
+
+    return n
