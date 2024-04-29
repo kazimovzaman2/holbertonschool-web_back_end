539a6fc4136246822e4a17bb3306c50c4d02bbc2
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-29 07:43:28 +0400

    Add doc

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,7 +4,9 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """"""
+    """
+    Sums up all the elements in the input_list and returns the total.
+    """
     n = 0
     for i in input_list:
         n += i
