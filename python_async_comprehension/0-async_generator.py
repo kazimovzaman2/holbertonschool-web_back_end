aa85175480d23091164ea98b4848a23b74754f90
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-30 14:33:26 +0400

    Task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python async generator task"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
