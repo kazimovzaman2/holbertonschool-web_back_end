fe2a4172ba6f0b060a336f517b85f6edea825365
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-30 14:36:14 +0400

    Return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Python async generator task"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """"""
     for _ in range(10):
         await asyncio.sleep(1)
