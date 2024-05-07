aeabe3649edd3d499a2842b0b66f87585921f48e
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-07 14:27:02 +0400

    Task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,11 +4,12 @@ import csv
 from typing import List
 
 
-index_range = __import__('0-simple_helper_function').index_range
+index_range = __import__("0-simple_helper_function").index_range
 
 
 class Server:
     """Server class to paginate"""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -31,3 +32,18 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get hypermedia pagination"""
+        data = self.get_page(page, page_size)
+        dataset = self.dataset()
+        total_pages = len(dataset) // page_size + (len(dataset) % page_size > 0)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
