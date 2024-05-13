78d98ad3a8491a7d63674881747666aa790ed135
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-13 15:11:56 +0400

    Use get method

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -2,8 +2,8 @@ import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
