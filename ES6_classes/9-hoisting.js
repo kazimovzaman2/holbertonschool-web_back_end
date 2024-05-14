6ed4a73a41ee364b1c3879644f23f8d580f9218d
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-14 14:52:02 +0400

    Not einstein but clinton

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -38,7 +38,7 @@ export class StudentHolberton {
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Einstein', class2019);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
